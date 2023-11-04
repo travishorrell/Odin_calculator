20b6f803d73a07b82aa83e4a28eb2534e726390c
travishorrell <17819559+travishorrell@users.noreply.github.com>
2023-11-03 22:43:51 -0600

    Updated JS to output button presses

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-let number1 = document.querySelectorAll('button');
-let number2 = document.querySelectorAll('button');
+const btn = document.querySelectorAll('button');
+let number1;
+let number2;
 let total;
+
+
+
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.id == "AC" || button.id == "C" || button.id == "BS")
+        {
+            alert("Cleared Numbers");
+        }
+        else
+        {
+            document.getElementById("mainScreenOutput").innerHTML = button.id;
+        }
+    })
+})
 
 
 
